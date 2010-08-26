df68baffe6433d82878ea3ea07375f25420aecdc
Serrano Pereira <figure002@gmail.com>
2010-08-26 20:58:22 +0200

    Renamed nextwall.py to nextwall. Added a setup.py so installation is possible.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (name='NextWall',
+      version='0.8',
+      description='A script to change to a random background image.',
+      author='Serrano Pereira',
+      author_email='figure002@gmail.com',
+      license='GPL3',
+      scripts=['nextwall'],
+)
