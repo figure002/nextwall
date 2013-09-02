b196dc82658fa041690f27181f1dab5190d35d9f
Serrano Pereira <serrano.pereira@gmail.com>
2013-09-02 19:48:59 +0200

    Comment sunriset.c's main()

--- src/sunriset.c
+++ src/sunriset.c
@@ -125,6 +125,116 @@ double GMST0( double d );
 
 
 
+/* A small test program
+
+main()
+{
+      int year,month,day;
+      double lon, lat;
+      double daylen, civlen, nautlen, astrlen;
+      double rise, set, civ_start, civ_end, naut_start, naut_end,
+             astr_start, astr_end;
+      int    rs, civ, naut, astr;
+      char buf[80];
+
+      printf( "Longitude (+ is east) and latitude (+ is north) : " );
+      fgets(buf, 80, stdin);
+      sscanf(buf, "%lf %lf", &lon, &lat );
+
+      for(;;)
+      {
+            printf( "Input date ( yyyy mm dd ) (ctrl-C exits): " );
+            fgets(buf, 80, stdin);
+            sscanf(buf, "%d %d %d", &year, &month, &day );
+
+            daylen  = day_length(year,month,day,lon,lat);
+            civlen  = day_civil_twilight_length(year,month,day,lon,lat);
+            nautlen = day_nautical_twilight_length(year,month,day,lon,lat);
+            astrlen = day_astronomical_twilight_length(year,month,day,
+                  lon,lat);
+
+            printf( "Day length:                 %5.2f hours\n", daylen );
+            printf( "With civil twilight         %5.2f hours\n", civlen );
+            printf( "With nautical twilight      %5.2f hours\n", nautlen );
+            printf( "With astronomical twilight  %5.2f hours\n", astrlen );
+            printf( "Length of twilight: civil   %5.2f hours\n",
+                  (civlen-daylen)/2.0);
+            printf( "                  nautical  %5.2f hours\n",
+                  (nautlen-daylen)/2.0);
+            printf( "              astronomical  %5.2f hours\n",
+                  (astrlen-daylen)/2.0);
+
+            rs   = sun_rise_set         ( year, month, day, lon, lat,
+                                          &rise, &set );
+            civ  = civil_twilight       ( year, month, day, lon, lat,
+                                          &civ_start, &civ_end );
+            naut = nautical_twilight    ( year, month, day, lon, lat,
+                                          &naut_start, &naut_end );
+            astr = astronomical_twilight( year, month, day, lon, lat,
+                                          &astr_start, &astr_end );
+
+            printf( "Sun at south %5.2fh UT\n", (rise+set)/2.0 );
+
+            switch( rs )
+            {
+                case 0:
+                    printf( "Sun rises %5.2fh UT, sets %5.2fh UT\n",
+                             rise, set );
+                    break;
+                case +1:
+                    printf( "Sun above horizon\n" );
+                    break;
+                case -1:
+                    printf( "Sun below horizon\n" );
+                    break;
+            }
+
+            switch( civ )
+            {
+                case 0:
+                    printf( "Civil twilight starts %5.2fh, "
+                            "ends %5.2fh UT\n", civ_start, civ_end );
+                    break;
+                case +1:
+                    printf( "Never darker than civil twilight\n" );
+                    break;
+                case -1:
+                    printf( "Never as bright as civil twilight\n" );
+                    break;
+            }
+
+            switch( naut )
+            {
+                case 0:
+                    printf( "Nautical twilight starts %5.2fh, "
+                            "ends %5.2fh UT\n", naut_start, naut_end );
+                    break;
+                case +1:
+                    printf( "Never darker than nautical twilight\n" );
+                    break;
+                case -1:
+                    printf( "Never as bright as nautical twilight\n" );
+                    break;
+            }
+
+            switch( astr )
+            {
+                case 0:
+                    printf( "Astronomical twilight starts %5.2fh, "
+                            "ends %5.2fh UT\n", astr_start, astr_end );
+                    break;
+                case +1:
+                    printf( "Never darker than astronomical twilight\n" );
+                    break;
+                case -1:
+                    printf( "Never as bright as astronomical twilight\n" );
+                    break;
+            }
+      return 0;
+      }
+}
+*/
+
 /* The "workhorse" function for sun rise/set times */
 
 int __sunriset__( int year, int month, int day, double lon, double lat,
